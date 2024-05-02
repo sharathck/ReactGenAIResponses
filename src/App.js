4d6846d9cd6d97e4cd4e9d94f316ba31d8d1ad38
Aarush <aarush@gmail.com>
2024-05-02 03:16:15 -0500

    Update asset manifest and index.html with new file versions

--- src/App.js
+++ src/App.js
@@ -2,30 +2,52 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 const urlParams = new URLSearchParams(window.location.search);
 const limit = urlParams.get("limit") || 6;
-const q = urlParams.get("q") || 'showrecent';
 const App = () => {
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+
   useEffect(() => {
+    console.log(searchQuery);
+    const q = searchQuery.replace(/ /g, '-');
+    setIsLoading(true);
+
     fetch(`https://us-central1-reviewtext-ad5c6.cloudfunctions.net/function-11?limit=${limit}&q=${q}`)
-    .then((res) => {
+      .then((res) => {
         return res.json();
       })
       .then((data) => {
         try {
-       //   const parsedData = JSON.parse(data);
+          //   const parsedData = JSON.parse(data);
           setData(data);
+          setIsLoading(false);
         } catch (error) {
           console.log("Invalid JSON format:", error);
+          setIsLoading(false);
         }
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [searchQuery]);
+
+  const handleSearchChange = (event) => {
+
+    setSearchQuery(event.target.value);
+
+
+  };
+
 
   return (
     <div>
+      <input style={{ width: "60%", boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)" }} type="text" onKeyDown={(event) => event.key === "Enter" && handleSearchChange(event)} placeholder="Search..." />
+      <div>
+      {isLoading && <p> Loading Data...</p>}
+      {/* Rest of your component */}
+      </div>
       {data.map((item) => (
         <div key={item.createdDateTime}>
-          <h3 style={{ color: "brown" }}> {item.createdDateTime} ---- model: <span style={{ color: "blue", fontSize: "24px" }}>{item.model}</span></h3>
+          <h3 style={{ color: "brown" }}> model: <span style={{ color: "blue", fontSize: "24px" }}>{item.model}</span></h3>
           <div style={{ border: "1px dotted black", padding: "2px" }}>
             <p style={{ color: "grey" }}>{item.question}</p>
           </div>
