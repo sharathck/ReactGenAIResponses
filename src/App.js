1b294e854276b62938018430a8e6e462d1516b25
Sharath <sharathck@outlook.com>
2024-05-02 14:06:22 -0500

    chore: Update TypeScript dependency to version 4.9.5 and Node.js version to >=4.2.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
-const urlParams = new URLSearchParams(window.location.search);
-const limit = urlParams.get("limit") || 6;
 const App = () => {
   const [data, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [limit, setLimit] = useState('6');
   const [isLoading, setIsLoading] = useState(false);
   const rowCount = data.length;
 
@@ -12,8 +11,12 @@ const App = () => {
     console.log(searchQuery);
     const q = searchQuery.replace(/ /g, '-');
     setIsLoading(true);
-
-    fetch(`https://us-central1-reviewtext-ad5c6.cloudfunctions.net/function-11?limit=${limit}&q=${q}`)
+    // set queryParameter string to &q=${q} if q is not empty
+    console.log('limit ' + limit); 
+    const queryParameter = q ? `&q=${q}` : '';
+    console.log('queryParametr ' + queryParameter);
+    console.log(`https://us-central1-reviewtext-ad5c6.cloudfunctions.net/function-11?limit=${limit}${queryParameter}`); 
+    fetch(`https://us-central1-reviewtext-ad5c6.cloudfunctions.net/function-11?limit=${limit}${queryParameter}`)
       .then((res) => {
         return res.json();
       })
@@ -28,24 +31,29 @@ const App = () => {
         }
       })
       .catch((err) => console.log(err));
-  }, [searchQuery]);
+  }, [limit, searchQuery]); // <-- Add the limit and searchQuery dependencies
 
   const handleSearchChange = (event) => {
-
     setSearchQuery(event.target.value);
-
-
   };
+
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
+
 
 
   return (
     <div>
-      <input style={{ width: "60%", boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)" }} type="text" onKeyDown={(event) => event.key === "Enter" && handleSearchChange(event)} placeholder="Search..." />
+      <input id="searchInput" style={{ fontSizex: "24px", height: "10%", width: "60%", boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)" }} type="text" onKeyDown={(event) => event.key === "Enter" && handleSearchChange(event)} placeholder="Search..." />
+      <label htmlFor="limitInput">    Rows Limit:</label>
+      <input id="limitInput" style={{width: "10%", boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)" }} type="text" onKeyDown={(event) => event.key === "Enter" && handleLimitChange(event)} placeholder="6"/>
       <div>
       {isLoading && <p> Loading Data...</p>}
       {/* Rest of your component */}
       </div>
-      <h5 style={{ color: "green" }}> Total Rows: {rowCount}</h5>
+      <h5 style={{ width: "30%",color: "green" }}> Fetched Rows: {rowCount}</h5>
+
       {data.map((item) => (
         <div key={item.createdDateTime}>
           <h3 style={{ color: "brown" }}> model: <span style={{ color: "blue", fontSize: "24px" }}>{item.model}</span></h3>
